7db6e39231534e0efcdd27ff5b6995e49f485286
Evan Carroll <me@evancarroll.com>
2009-04-24 18:46:13 -0500

    working with Moose!! (haven't fucked with new)

--- lib/POE/Filter/Stream.pm
+++ lib/POE/Filter/Stream.pm
@@ -1,13 +1,11 @@
 # $Id: Stream.pm 2447 2009-02-17 05:04:43Z rcaputo $
-
 package POE::Filter::Stream;
+use Moose;
 
-use strict;
-use POE::Filter;
+with 'POE::Filter';
 
 use vars qw($VERSION @ISA);
 $VERSION = do {my($r)=(q$Revision: 2447 $=~/(\d+)/);sprintf"1.%04d",$r};
-@ISA = qw(POE::Filter);
 
 sub new {
 	my $type = shift;
