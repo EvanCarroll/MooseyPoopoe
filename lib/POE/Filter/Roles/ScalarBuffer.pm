6adacc5c8f92a4da01d9d02288151c86b52938b8
Evan Carroll <me@evancarroll.com>
2009-04-26 22:29:42 -0500

    added a debug statement

--- lib/POE/Filter/Roles/ScalarBuffer.pm
+++ lib/POE/Filter/Roles/ScalarBuffer.pm
@@ -21,6 +21,18 @@ has 'buffer' => (
 );
 
 sub get_one_start { $_[0]->append_to_buffer( join '', @{$_[1]} ); }
+  
+
+=for comment
+
+	This was in the old Line.pm's get_one_start, which had a static DEBUG flag 
+	DEBUG and do {
+    my $temp = join '', @$stream;
+    $temp = unpack 'H*', $temp;
+    warn "got some raw data: $temp\n";
+  };
+
+=cut
 
 sub get_pending {
 	my $self = shift;
